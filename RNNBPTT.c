78f5e15246c8875eca82fb561018e6803cfdf68e
John-Anthony-Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-09-27 12:01:50 +0100

    created function to initialise the random starting weights

--- RNNBPTT.c
+++ RNNBPTT.c
@@ -27,3 +27,24 @@ typedef struct {
     double b_o[OUTPUT_SIZE]; 
     double h[TIME_STEPS][HIDDEN_SIZE]; 
 } RNN;
+
+void initialize_rnn(RNN *rnn) {
+    for (int i = 0; i < INPUT_SIZE; i++)
+        for (int j = 0; j < HIDDEN_SIZE; j++)
+            rnn->W_ih[i][j] = ((double) rand() / RAND_MAX) * 0.2 - 0.1;
+
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+        for (int j = 0; j < HIDDEN_SIZE; j++)
+            rnn->W_hh[i][j] = ((double) rand() / RAND_MAX) * 0.2 - 0.1;
+
+    for (int i = 0; i < HIDDEN_SIZE; i++)
+        for (int j = 0; j < OUTPUT_SIZE; j++)
+            rnn->W_ho[i][j] = ((double) rand() / RAND_MAX) * 0.2 - 0.1;
+
+    for (int i = 0; i < HIDDEN_SIZE; i++) {
+        rnn->b_h[i] = 0.0;
+    }
+    for (int i = 0; i < OUTPUT_SIZE; i++) {
+        rnn->b_o[i] = 0.0;
+    }
+}
